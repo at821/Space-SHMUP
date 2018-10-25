6f8eb91bb3b9c751d3bbbb33e33fdb033bd7c6ff
at821 <36107231+at821@users.noreply.github.com>
2018-10-25 18:25:45 -0400

    692
    same issues

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -23,6 +23,10 @@ public class Hero : MonoBehaviour {
 
     private GameObject lastTriggerGo = null;
 
+    public delegate void WeaponFireDelegate();
+
+    public WeaponFireDelegate fireDelegate;
+
     void Awake ()    {
         if (S == null)        {
             S = this;
@@ -30,6 +34,7 @@ public class Hero : MonoBehaviour {
         else        {
             Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
         }
+        fireDelegate += TempFire;
     }
 
 	// Use this for initialization
@@ -49,8 +54,12 @@ public class Hero : MonoBehaviour {
 
         transform.rotation = Quaternion.Euler(yAxis + pitchMult, xAxis * rollMult, 0);
 
-        if (Input.GetKeyDown(KeyCode.Space))        {
-            TempFire();
+        //if (Input.GetKeyDown(KeyCode.Space))        {
+            //TempFire();
+        //}
+
+        if(Input.GetAxis("Jump") ==1 && fireDelegate != null)        {
+            fireDelegate();
         }
 
 	}
@@ -59,7 +68,13 @@ public class Hero : MonoBehaviour {
         GameObject projGO = Instantiate<GameObject>(projectilePrefab);
         projGO.transform.position = transform.position;
         Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
-        rigidB.velocity = Vector3.up * projectileSpeed;
+        //rigidB.velocity = Vector3.up * projectileSpeed;
+
+        projectilePrefab proj = projGO.GetComponent<projectilePrefab>();
+        porj.type = WeaponType.blaster;
+        float tSpeed = Main.GetWeaponDefinition(proj.type).velocity;
+        rigidB.velocity = Vector3.up * tSpeed;
+
     }
 
     void OnTriggerEnter (Collider other)    {
