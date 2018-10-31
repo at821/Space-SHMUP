b0052277c8f9f11376cd84e522c3275a9e1629a7
at821 <36107231+at821@users.noreply.github.com>
2018-10-30 23:20:25 -0400

    701
    Utils created

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -49,6 +49,24 @@ public class Enemy : MonoBehaviour {
 
     void OnCollisionEnter (Collision coll)    {
         GameObject otherGO = coll.gameObject;
+        switch (otherGO.tag)        {
+            case "PreojectileHero":
+                Projectile p = otherGO.GetComponent<Projectile>();
+                if (!bndCheck.isOnScreen)                {
+                    Destroy(otherGO);
+                    break;
+                }
+            health -= Main.GetWeaponDefinition(p.type).damageOnHit;
+                if(health <= 0)                {
+                    Destroy(this.gameObject);
+                }
+                Destroy(otherGO);
+                break;
+
+            default:
+                print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+                break;
+        }
 
         if (otherGO.tag =="ProjectileHero")        {
             Destroy(otherGO);
